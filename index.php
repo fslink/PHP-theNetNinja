a1a6751ce90a41c09b4ca4cf6e2f1c26943423a4
FS_link <mowglysofiann@gmail.com>
2020-03-23 21:38:23 +0100

    Variables & Constants

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    define('NAME', 'Jedi');
+    $name = 'soso';
+    $age = 32;
+    echo $name;
+    define('NAME', 'Denis');
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -5,6 +13,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo 'Hello, World'; ?></h1>
+    <h1><?php echo 'Hello, $name'; ?></h1>
+    <h2><?php echo NAME, ' ', $age; ?></h2>
 </body>
 </html>
