cf9e9fc4a2fee6c638144ee68b17339b6ec4f61d
FS_link <mowglysofiann@gmail.com>
2020-04-13 01:23:39 +0200

    #14 - Variable Scope

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php
 
-$products = [
-    ['name' => 'shiny star', 'price' => 20],
-    ['name' => 'green shell', 'price' => 10],
-    ['name' => 'red shell', 'price' => 15],
-    ['name' => 'gold coin', 'price' => 5],
-    ['name' => 'lightning bolt', 'price' => 40],
-    ['name' => 'banana skin', 'price' => 2]
-];
+// $products = [
+//     ['name' => 'shiny star', 'price' => 20],
+//     ['name' => 'green shell', 'price' => 10],
+//     ['name' => 'red shell', 'price' => 15],
+//     ['name' => 'gold coin', 'price' => 5],
+//     ['name' => 'lightning bolt', 'price' => 40],
+//     ['name' => 'banana skin', 'price' => 2]
+// ];
 
-function sayHello($name = 'mario'){
-    echo "Salut $name ma petite gueule !";
+// function sayHello($name = 'mario'){
+//     echo "Salut $name ma petite gueule !";
+// }
+
+// // sayHello();
+
+// function formattedProduct($product){
+//     // echo "The product {$product['name']} costs {$product['price']} to buy";
+//     return "The product {$product['name']} costs {$product['price']} to buy </br>";
+// }
+
+// $formatted[] = formattedProduct($products[0]);
+// $formatted[] = formattedProduct(['name' => 'blue shell', 'price' => 25]);
+
+// echo $formatted[0];
+// echo $formatted[1];
+
+$name = 'mario';
+
+function sayHello(&$name){
+    $name = 'wario';
+    echo "Hello mister $name </br>";
 }
 
-// sayHello();
-
-function formattedProduct($product){
-    // echo "The product {$product['name']} costs {$product['price']} to buy";
-    return "The product {$product['name']} costs {$product['price']} to buy </br>";
-}
-
-$formatted[] = formattedProduct($products[0]);
-$formatted[] = formattedProduct(['name' => 'blue shell', 'price' => 25]);
-
-echo $formatted[0];
-echo $formatted[1];
+sayHello($name);
+echo $name;
 
 ?>
 
