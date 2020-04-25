07e9449394ca8a3655555270030b11b715ec4ac9
FS_link <mowglysofiann@gmail.com>
2020-04-26 01:35:27 +0200

    Project Header & Footer

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
-<?php
-    //include('ninjas.php');
-    //require('ninjass.php');
-
-    echo 'end of php </br>';
-?>
-
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Tutorial NetNinja</title>
-</head>
-<body>
-    <?php include 'content.php'; ?>
-    <p>Hello Wolrd</p>
-</body>
+    <!-- HEADER  -->
+    <?php include 'templates/header.php'; ?>
+    
+    <!-- FOOTER -->
+    <?php include 'templates/footer.php'; ?>
 </html>
-
-<?php
-include('ninjas.php');
-?>
---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial NetNinja</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <style>
+        .brand{
+            background: #cbb09c !important;
+        }
+
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text center">Ninja Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+            <li><a href="#" class="btn brand z-depth-0">new pizza</a></li>
+            </ul>
+        </div>
+    </nav>
